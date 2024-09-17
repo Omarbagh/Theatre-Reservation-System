f8245ff3d56f82fddabfa2048d16a594b12889f0
Omarbagh <o.elbaghouri@icloud.com>
2024-09-17 22:18:54 +0200

    Database aangemaakt.

---
+++ Migrations/20240917201351_InitialCreate.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StarterKit.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admin",
+                columns: table => new
+                {
+                    AdminId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserName = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admin", x => x.AdminId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: true),
+                    LastName = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Venue",
+                columns: table => new
+                {
+                    VenueId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Capacity = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Venue", x => x.VenueId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TheatreShow",
+                columns: table => new
+                {
+                    TheatreShowId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<double>(type: "REAL", nullable: false),
+                    VenueId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TheatreShow", x => x.TheatreShowId);
+                    table.ForeignKey(
+                        name: "FK_TheatreShow_Venue_VenueId",
+                        column: x => x.VenueId,
+                        principalTable: "Venue",
+                        principalColumn: "VenueId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TheatreShowDate",
+                columns: table => new
+                {
+                    TheatreShowDateId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DateAndTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    TheatreShowId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TheatreShowDate", x => x.TheatreShowDateId);
+                    table.ForeignKey(
+                        name: "FK_TheatreShowDate_TheatreShow_TheatreShowId",
+                        column: x => x.TheatreShowId,
+                        principalTable: "TheatreShow",
+                        principalColumn: "TheatreShowId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservation",
+                columns: table => new
+                {
+                    ReservationId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AmountOfTickets = table.Column<int>(type: "INTEGER", nullable: false),
+                    Used = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CustomerId = table.Column<int>(type: "INTEGER", nullable: true),
+                    TheatreShowDateId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservation", x => x.ReservationId);
+                    table.ForeignKey(
+                        name: "FK_Reservation_Customer_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customer",
+                        principalColumn: "CustomerId");
+                    table.ForeignKey(
+                        name: "FK_Reservation_TheatreShowDate_TheatreShowDateId",
+                        column: x => x.TheatreShowDateId,
+                        principalTable: "TheatreShowDate",
+                        principalColumn: "TheatreShowDateId");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Admin",
+                columns: new[] { "AdminId", "Email", "Password", "UserName" },
+                values: new object[,]
+                {
+                    { 1, "admin1@example.com", "^�H��(qQ��o��)'s`=\rj���*�rB�", "admin1" },
+                    { 2, "admin2@example.com", "\\N@6��G��Ae=j_��a%0�QU��\\", "admin2" },
+                    { 3, "admin3@example.com", "�j\\��f������x�s+2��D�o���", "admin3" },
+                    { 4, "admin4@example.com", "�].��g��Պ��t��?��^�T��`aǳ", "admin4" },
+                    { 5, "admin5@example.com", "E�=���:�-����gd����bF��80]�", "admin5" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Admin_UserName",
+                table: "Admin",
+                column: "UserName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservation_CustomerId",
+                table: "Reservation",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservation_TheatreShowDateId",
+                table: "Reservation",
+                column: "TheatreShowDateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TheatreShow_VenueId",
+                table: "TheatreShow",
+                column: "VenueId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TheatreShowDate_TheatreShowId",
+                table: "TheatreShowDate",
+                column: "TheatreShowId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admin");
+
+            migrationBuilder.DropTable(
+                name: "Reservation");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "TheatreShowDate");
+
+            migrationBuilder.DropTable(
+                name: "TheatreShow");
+
+            migrationBuilder.DropTable(
+                name: "Venue");
+        }
+    }
+}
--- Models/DatabaseContext.cs
+++ Models/DatabaseContext.cs
@@ -1,35 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using StarterKit.Utils;
 
+using Microsoft.EntityFrameworkCore;
+
 namespace StarterKit.Models
 {
     public class DatabaseContext : DbContext
     {
-        // The admin table will be used in both cases
         public DbSet<Admin> Admin { get; set; }
-
-        // You can comment out or remove the case you are not going to use.
-
-        // Tables for the Theatre ticket case
-
-        // public DbSet<Customer> Customer { get; set; }
-        // public DbSet<Reservation> Reservation { get; set; }
-        // public DbSet<TheatreShowDate> TheatreShowDate { get; set; }
-        // public DbSet<TheatreShow> TheatreShow { get; set; }
-        // public DbSet<Venue> Venue { get; set; }
-
-        // Tables for the event calendar case
-
-        // public DbSet<User> User { get; set; }
-        // public DbSet<Attendance> Attendance { get; set; }
-        // public DbSet<Event_Attendance> Event_Attendance { get; set; }
-        // public DbSet<Event> Event { get; set; }
-
-
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Reservation> Reservation { get; set; }
+        public DbSet<TheatreShowDate> TheatreShowDate { get; set; }
+        public DbSet<TheatreShow> TheatreShow { get; set; }
+        public DbSet<Venue> Venue { get; set; }
 
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
         {
-
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -48,7 +34,5 @@ namespace StarterKit.Models
             modelBuilder.Entity<Admin>()
                 .HasData(new Admin { AdminId = 5, Email = "admin5@example.com", UserName = "admin5", Password = EncryptionHelper.EncryptPassword("Whatisapassword?") });
         }
-
     }
-
 }
