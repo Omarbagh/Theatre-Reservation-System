4325c5ad1121e877bcd2d8e09dbeca6b7c5a092e
Omarbagh <o.elbaghouri@icloud.com>
2024-10-08 13:03:06 +0200

    title aangepast

--- Controllers/ShowController.cs
+++ Controllers/ShowController.cs
@@ -144,7 +144,7 @@ public class ShowController : ControllerBase
 
 
     [HttpGet("filter")]
-    public async Task<IActionResult> Filter(string filter)
+    public async Task<IActionResult> FilteronLocation(string filter)
     {
         // Assuming _context is your database context
         var shows = await _context.TheatreShow.ToListAsync();
