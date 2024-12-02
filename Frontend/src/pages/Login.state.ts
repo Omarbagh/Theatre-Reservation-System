86979be9e212ea52a7ee1a40e86d13a6de6504fc
JadAdlaoui <Jadadlaoui2006@hotmail.com>
2024-12-02 17:35:05 +0100

    login react

---
+++ Frontend/src/pages/Login.state.ts
@@ -0,0 +1,26 @@
+export interface Login {
+    Username: string;
+    Password: string;
+}
+
+export type LoginState = Login & {
+    updateUserName: (name: string) => (state: LoginState) => LoginState;
+    updatePassword: (name: string) => (state: LoginState) => LoginState;
+};
+
+export const initLogState: LoginState = {
+    Username: "Enter username",
+    Password: "Enter password",
+    updateUserName: (name: string) => (state: LoginState): LoginState => ({
+        ...state,
+        Username: name,
+    }),
+    updatePassword: (name: string) => (state: LoginState): LoginState => ({
+        ...state,
+        Password: name,
+    }),
+};
+
+export interface LoginProps {
+    insertLogin: (login: Login) => void;
+}
